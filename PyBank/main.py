3be8a28c2631aef7b7c7ac8eab7fc6cc71fbd862
Sntsjrg <sntsjrg@yahoo.com>
2023-10-18 16:03:39 -0400

    PyBank solution

--- PyBank/main.py
+++ PyBank/main.py
@@ -37,12 +37,23 @@ with open(csvpath) as csvfile:
 total_change = sum(loss_changes)
 average_change = total_change/(total_months-1)
 
+# Find the greatest increase and decrease in profits
+max_increase = max(loss_changes)
+max_increase_index = loss_changes.index(max_increase)
+max_increase_date = dates[max_increase_index]
+
+max_decrease = min(loss_changes)
+max_decrease_index = loss_changes.index(max_decrease)
+max_decrease_date = dates[max_decrease_index]
+
 # Print the analysis results
 print("Financial Analysis")
 print("-------------------------")
 print(f"Total Months: {total_months}")
 print(f"Total: ${net_total}")
 print(f"Average Change: ${average_change:.2f}")
+print(f"Greatest Increase in Profits: {max_increase_date} (${max_increase})")
+print(f"Greatest Decrease in Profits: {max_decrease_date} (${max_decrease})")
 
 # Specify the file to write to
 output_path = os.path.join("Analysis", "new.txt")
@@ -57,3 +68,5 @@ with open(output_path, 'w', newline ="") as outputfile:
     writer.writerow([f"Total Months: {total_months}"])
     writer.writerow([f"Total: ${net_total}"])
     writer.writerow([f"Average Change: ${average_change:.2f}"])
+    writer.writerow([f"Greatest Increase in Profits: {max_increase_date} (${max_increase})"])
+    writer.writerow([f"Greatest Decrease in Profits: {max_decrease_date} (${max_decrease})"])
